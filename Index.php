cc959868538880bdd7cfa897750d37506f83857b
Clarenaliaka <yalekky@gmail.com>
2014-09-29 13:52:52 +0300

    second

--- Index.php
+++ Index.php
@@ -13,27 +13,34 @@ $startups = $db-> get('start');
 <!DOCTYPE html>
 <html
 <head>
-<title>nairobi</title>
+<title>Nairobi</title>
 <link href="assets/css/main.css" rel="stylesheet" type="text/css"/>   
- <meta name="viewport" content="width=device-width, initial-scale=1" />       
+<meta name="viewport" content="width=device-width, initial-scale=1" />       
 </head>
 <body>
-<?php include 'want.php'; ?>
-	<article>
-<ul class="startups">
-	<?php foreach($startups as $startup): ?>
-		<?php if($startup['company_logo'] != ''): ?>
-		    <li>
-				<a href="<?php echo $startup['website']; ?>" target="_blank">
 
-					<img src="assets/Image/<?php echo $startup['company_logo']; ?>" alt="<?php echo $startup['company_name'] ?>" />
-				</a>
-			</li>
-		<?php endif; ?>
-    <?php endforeach; ?>
-</ul>
+<?php include 'want.php';?>
+
+<article>
+	<ul class="startups">
+		<?php foreach($startups as $startup): ?>
+			<?php if($startup['company_logo'] != ''): ?>
+			    <li>
+					<a href="<?php echo $startup['website']; ?>" target="_blank">
+
+						<img src="assets/Image/<?php echo $startup['company_logo']; ?>" alt="<?php echo $startup['company_name'] ?>" />
+					</a>
+					<div class="hover">
+
+						<a href="<?php echo $startup['website']; ?>" target="_blank"><h2>View Website</h2>
+						
+					</div>
+				</li>
+			<?php endif; ?>
+	    <?php endforeach; ?>
+	</ul>
 </article>
-</div>
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script> 
 <script type="text/javascript" >
